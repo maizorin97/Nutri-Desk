441ee4e2a2690dece4788a11719cecaaad9a14e0
Ismael Zavala <maizorin97@gmail.com>
2020-03-15 18:27:21 -0600

    se agrego el panel de control, se agrego una vista para modificar los datos del usuario

--- back-end/nutridesk/core/urls.py
+++ back-end/nutridesk/core/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index.as_view(), name='index'),
+    path('panel/', views.panel_control.as_view(), name='panel'),
     path('registro/', views.registro.as_view(), name='registro'),
     path('ingresar/', views.ingresar.as_view(), name='ingresar'),
+    path('perfil/<int:id>/', views.perfil.as_view(), name='perfil'),
     path('cerrar/', views.cerrar.as_view(), name='cerrar'),
     path('error/', views.error.as_view(), name='error'),
 ]
--- back-end/nutridesk/core/views.py
+++ back-end/nutridesk/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, UpdateView
 from django.contrib.auth.views import LoginView, LogoutView
 
 from django.urls import reverse_lazy
@@ -30,9 +30,19 @@ class registro(CreateView):
     def get_success_url(self):
         return super().get_success_url()
 
+class panel_control(TemplateView):
+    template_name = "panel_control.html"
+
+class perfil(UpdateView):
+    model = models.User
+    template_name = "perfil.html"
+    pk_url_kwarg = 'id' 
+    fields = ['first_name','last_name','password']
+    success_url = "/panel"
+    
+
 class cerrar(LogoutView):
     template_name = "cerrar.html"
-
 
 class error(TemplateView):
     template_name = "error.html"
