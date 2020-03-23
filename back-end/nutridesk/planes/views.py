4ab7b2ffbdb2969dce4701873c43311929b44551
Ismael Zavala <maizorin97@gmail.com>
2020-03-23 13:11:39 -0600

    primera aproximacion al guardado del plan alimenticio (antes de proceder mejorar el codigo js en generar_plan.html)

--- back-end/nutridesk/planes/views.py
+++ back-end/nutridesk/planes/views.py
@@ -1,17 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.views.generic import CreateView, ListView, TemplateView
 
 from .models import Plan
-
-# Create your views here.
-"""class crear_plan(CreateView):
-    template_name = "registro.html"
-    form_class = forms.FormaRegistroUsuario
-    success_url = "/ingresar"
-
-    def get_success_url(self):
-        return super().get_success_url()"""
 
 class lista_planes(ListView):
     template_name = "lista_planes.html"
@@ -20,10 +11,10 @@ class lista_planes(ListView):
 
 def generar_planes(request):
     if request.method == 'POST':
-        pass
-        """user_form = forms.FormaRegistroUsuario(request.POST)
-        info_form = forms.FormaDatosFisiologicos(request.POST)
-        if user_form.is_valid() and info_form.is_valid():
+        guardar_plan(request.POST)
+        return redirect("ingresar")
+        #valida
+        """if user_form.is_valid() and info_form.is_valid():
             user = user_form.save()
             info = info_form.save(commit=False)
             info.usuario = user
@@ -33,12 +24,29 @@ def generar_planes(request):
         else:
             return render(request, "registro.html",{'user_form':user_form,'info_form':info_form})"""
     else:
-        current_user = request.user
-        from core.models import InfoUsuario
-        from smae.models import Alimento, Grupo
-        infoUser = InfoUsuario.objects.get(usuario=current_user)#filter(usuario=current_user)#.all()
-        listaGrupos = Grupo.objects.all()
-        listaAlimentos = Alimento.objects.all()
-        from datetime import datetime as dt
-        edad = dt.now().year - infoUser.fecha_nacimiento.year + (dt.now().month - infoUser.fecha_nacimiento.month)*(1/12)
-        return render(request,'generar_plan.html',{'infoUser':infoUser,'edad':edad,'listaAlimentos':listaAlimentos,'listaGrupos':listaGrupos})
+        return preparar_plan(request.user, request)
+
+
+def preparar_plan(current_user, request):
+    from core.models import InfoUsuario
+    from smae.models import Alimento, Grupo
+    from datetime import datetime as dt
+    
+    #filter(usuario=current_user)#.all()
+    infoUser = InfoUsuario.objects.get(usuario=current_user)
+    listaGrupos = Grupo.objects.all()
+    listaAlimentos = Alimento.objects.all()
+    edad = dt.now().year - infoUser.fecha_nacimiento.year + (dt.now().month - infoUser.fecha_nacimiento.month)*(1/12)
+
+    return render(request,'generar_plan.html',{'infoUser':infoUser,'edad':edad,'listaAlimentos':listaAlimentos,'listaGrupos':listaGrupos})
+
+
+def guardar_plan(req_post):
+    nombre_plan = req_post.get("txtNombrePlan")
+    aporte_kcal = int(req_post.get("txtAporteCalorico"))
+    print("nombre=" + nombre_plan + " kcal=" + str(aporte_kcal))
+    llave_comida = ("Desayuno","Colacion1","Comida","Colacion2","Cena")
+    comidas = []
+    for comida in llave_comida:
+        comidas.append(req_post.getlist(comida))
+    print(str(comidas))
