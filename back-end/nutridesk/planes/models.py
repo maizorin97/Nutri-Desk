b52b98123d36715ca5a371deaacd8619de1fb56d
Ismael Zavala <maizorin97@gmail.com>
2020-03-24 11:52:00 -0600

    se implemento una funcion en py para guardar los planes

--- back-end/nutridesk/planes/models.py
+++ back-end/nutridesk/planes/models.py
@@ -3,39 +3,50 @@ from django.contrib.auth.models import User
 
 from smae.models import Alimento
 
+
 class Plan(models.Model):
     idPlan = models.AutoField(primary_key=True, verbose_name="Id Plan")
-    idUsuario = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name="Id Usuario")
+    idUsuario = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Id Usuario"
+    )
     descripcion = models.CharField(verbose_name="Descripción", max_length=500)
     calorias = models.IntegerField(verbose_name="Calorías")
 
     class Meta:
-        ordering = ('-idUsuario',)
+        ordering = ("-idUsuario",)
         verbose_name_plural = "Planes"
 
     def __str__(self):
-        return 'plan {0} de {1}'.format(self.idPlan,self.idUsuario.username)
+        return "plan {0} de {1}".format(self.idPlan, self.idUsuario.username)
+
 
 class TipoComida(models.Model):
-    idTipoComida= models.AutoField(primary_key=True, verbose_name="Id Tipo Comida")
+    idTipoComida = models.AutoField(primary_key=True, verbose_name="Id Tipo Comida")
     nombre = models.CharField(verbose_name="Nombre", max_length=20)
 
     class Meta:
-        ordering = ('-idTipoComida',)
+        ordering = ("-idTipoComida",)
         verbose_name_plural = "Tipos de Comidas"
 
     def __str__(self):
-        return '{0}'.format(self.nombre)
+        return "{0}".format(self.nombre)
+
 
 class Colacion(models.Model):
     idColacion = models.AutoField(primary_key=True, verbose_name="Id Colacion")
-    idPlan = models.ForeignKey(Plan,on_delete=models.CASCADE,verbose_name="Id Plan")
-    idAlimento = models.ForeignKey(Alimento,on_delete=models.CASCADE,verbose_name="Id Alimento")
-    idTipoComida = models.ForeignKey(TipoComida,on_delete=models.CASCADE,verbose_name="Id Tipo Comida")
+    idPlan = models.ForeignKey(Plan, on_delete=models.CASCADE, verbose_name="Id Plan")
+    idAlimento = models.ForeignKey(
+        Alimento, on_delete=models.CASCADE, verbose_name="Id Alimento"
+    )
+    idTipoComida = models.ForeignKey(
+        TipoComida, on_delete=models.CASCADE, verbose_name="Id Tipo Comida"
+    )
 
     class Meta:
-        ordering = ('-idPlan',)
+        ordering = ("-idPlan",)
         verbose_name_plural = "Colaciones"
 
     def __str__(self):
-        return 'plan {0} -> {1}, {2}'.format(self.idPlan, self.idAlimento, self.idTipoComida)
+        return "plan {0} -> {1}, {2}".format(
+            self.idPlan, self.idAlimento, self.idTipoComida
+        )
--- back-end/nutridesk/planes/views.py
+++ back-end/nutridesk/planes/views.py
@@ -2,18 +2,26 @@ from django.shortcuts import render, redirect
 
 from django.views.generic import CreateView, ListView, TemplateView
 
-from .models import Plan
+from .models import Plan, TipoComida, Colacion
+from core.models import InfoUsuario
+from smae.models import Alimento, Grupo
+from django.contrib.auth.models import User
+
+from datetime import datetime as dt
+
 
 class lista_planes(ListView):
     template_name = "lista_planes.html"
     model = Plan
-    context_object_name = 'lista_planes'
+    context_object_name = "lista_planes"
+
 
 def generar_planes(request):
-    if request.method == 'POST':
-        guardar_plan(request.POST)
-        return redirect("ingresar")
-        #valida
+    if request.method == "POST":
+        usuario: User = InfoUsuario.objects.get(usuario=request.user).usuario
+        guardar_plan(request.POST, usuario)
+        return redirect("planes")
+        # valida
         """if user_form.is_valid() and info_form.is_valid():
             user = user_form.save()
             info = info_form.save(commit=False)
@@ -28,25 +36,54 @@ def generar_planes(request):
 
 
 def preparar_plan(current_user, request):
-    from core.models import InfoUsuario
-    from smae.models import Alimento, Grupo
-    from datetime import datetime as dt
-    
-    #filter(usuario=current_user)#.all()
+    # filter(usuario=current_user)#.all()
     infoUser = InfoUsuario.objects.get(usuario=current_user)
+    print(infoUser)
     listaGrupos = Grupo.objects.all()
     listaAlimentos = Alimento.objects.all()
-    edad = dt.now().year - infoUser.fecha_nacimiento.year + (dt.now().month - infoUser.fecha_nacimiento.month)*(1/12)
+    edad = (
+        dt.now().year
+        - infoUser.fecha_nacimiento.year
+        + (dt.now().month - infoUser.fecha_nacimiento.month) * (1 / 12)
+    )
 
-    return render(request,'generar_plan.html',{'infoUser':infoUser,'edad':edad,'listaAlimentos':listaAlimentos,'listaGrupos':listaGrupos})
+    return render(
+        request,
+        "generar_plan.html",
+        {
+            "infoUser": infoUser,
+            "edad": edad,
+            "listaAlimentos": listaAlimentos,
+            "listaGrupos": listaGrupos,
+        },
+    )
 
 
-def guardar_plan(req_post):
+def guardar_plan(req_post, current_user):
+
     nombre_plan = req_post.get("txtNombrePlan")
     aporte_kcal = int(req_post.get("txtAporteCalorico"))
     print("nombre=" + nombre_plan + " kcal=" + str(aporte_kcal))
-    llave_comida = ("Desayuno","Colacion1","Comida","Colacion2","Cena")
+
+    nuevo_plan: Plan = Plan(
+        idUsuario=current_user, descripcion=nombre_plan, calorias=aporte_kcal
+    )
+    nuevo_plan.save()
+
+    llave_comida = ("Desayuno", "Colacion1", "Comida", "Colacion2", "Cena")
     comidas = []
     for comida in llave_comida:
         comidas.append(req_post.getlist(comida))
+
+    i:int = 1
+    for comida in comidas:
+        tipo_comida: TipoComida = TipoComida.objects.get(idTipoComida=i)
+        print(i)
+        for id_alimento in comida:
+            alimento: Alimento = Alimento.objects.get(idAlimento=id_alimento)
+            colacion: Colacion = Colacion(
+                idAlimento=alimento, idPlan=nuevo_plan, idTipoComida=tipo_comida
+            )
+            colacion.save()
+        i += 1
     print(str(comidas))
